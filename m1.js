fb16fbd805f5d152f288dd0463ca475bb9f39731
tam11a <ibrahimsadiktamim@gmail.com>
2020-07-07 12:38:49 +0600

    first commit

---
+++ m1.js
@@ -0,0 +1,6 @@
+function toggle(){
+    var play = document.getElementById("play");
+    var nav = document.getElementById("navigation");
+    play.classList.toggle('on');
+    nav.classList.toggle('on');
+}
